ae7348005144131eb4851d2f4d296a6a07b47e77
Ayad <ayadlawrani65@gmail.com>
2024-08-25 22:25:34 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import Hero from '../components/CustomButton'
+import CustomButton from './CustomButton'
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer
+};
